6ed5ada69e93cf23b5d8692986b8481dd0eb97d8
Andreas Skyman <andreas.skyman@molflow.com>
2018-01-29 14:47:35 +0100

    @detrez comments

--- scanomaticd/heartbeat.py
+++ scanomaticd/heartbeat.py
@@ -1,20 +1,19 @@
 import logging
 from collections import namedtuple
 
-logging.basicConfig(format='%(asctime)s %(levelname)s %(message)s')
+from .apigateway import APIError
+
 LOG = logging.getLogger(__name__)
-LOG.setLevel(logging.DEBUG)
 
 
 class HeartbeatCommand:
-    OK_STATUS = {HTTPStatus.OK, HTTPStatus.CREATED}
 
     def __init__(self, apigateway):
         self._apigateway = apigateway
 
     def __call__(self, daemon):
         try:
-            response_code = self._apigateway.update_status(
+            self._apigateway.update_status(
                 job=daemon.get_scanning_job()
             )
         except APIError as error:
