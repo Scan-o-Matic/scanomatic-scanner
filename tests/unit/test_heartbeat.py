b696b1e68a70084ce3388f09809c50071b2517e9
Andreas Skyman <andreas.skyman@molflow.com>
2018-01-30 11:24:48 +0100

    removed rather pointless parametrization

--- tests/unit/test_heartbeat.py
+++ tests/unit/test_heartbeat.py
@@ -7,10 +7,8 @@ from scanomaticd.heartbeat import HeartbeatCommand
 
 class TestScannerHeartbeat:
 
-    @pytest.mark.parametrize("status", [HTTPStatus.OK, HTTPStatus.CREATED])
     def test_heartbeat_good(self, status):
         apigateway = MagicMock()
-        apigateway.update_status.return_value = status
         daemon = MagicMock()
         daemon.get_scanning_job.return_value = None
         heartbeat = HeartbeatCommand(apigateway)
