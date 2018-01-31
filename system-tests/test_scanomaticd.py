1ab7dfdc128e5b98a121e4fafbafedb6c5ed8b1a
Grégoire Détrez <gregoire.detrez@molflow.com>
2018-01-31 20:17:08 +0100

    Update the system test to use the API
    - Generate a random id for the scanner
    - Wait until the scanner appear in the API (first status update)
    - Get the scanning times from the api instead of looking at when files appear
    
    This means that the test now checks the whole process of
    registration (status update), scanning and uploading.

--- system-tests/test_scanomaticd.py
+++ system-tests/test_scanomaticd.py
@@ -1,14 +1,14 @@
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, timezone
 from glob import glob
+from http import HTTPStatus
 import time
 import uuid
 import os
 
 import docker
 import pytest
-import requests
+from requests import Session
 
-SCANDURATION = timedelta(minutes=3)
 APIROOT = 'https://som.molflow.com/api'
 USERNAME = os.environ['SCANOMATIC_USERNAME']
 PASSWORD = os.environ['SCANOMATIC_PASSWORD']
@@ -16,39 +16,11 @@ PASSWORD = os.environ['SCANOMATIC_PASSWORD']
 
 @pytest.fixture
 def scannerid():
-    return '9a8486a6f9cb11e7ac660050b68338ac'
+    return 'test-scanner:{}'.format(uuid.uuid4())
 
 
 @pytest.fixture
-def scanjobid(scannerid):
-    response = requests.post(
-        APIROOT + '/scan-jobs',
-        json={
-            'name': uuid.uuid1().hex, 'interval': 300, 'duration': 660,
-            'scannerId': scannerid,
-        },
-        auth=(USERNAME, PASSWORD),
-    )
-    response.raise_for_status()
-    jobid = response.json()['identifier']
-    requests.post(
-        APIROOT + '/scan-jobs/{}/start'.format(jobid),
-        auth=(USERNAME, PASSWORD),
-    ).raise_for_status()
-    return jobid
-
-
-def get_scan_times(jobid):
-    response = requests.get(APIROOT + '/scans', auth=(USERNAME, PASSWORD))
-    response.raise_for_status()
-    return [
-        datetime.strptime(obj['startTime'], '%Y-%m-%dT%H:%M:%SZ')
-        for obj in response if obj['scanJobId'] == jobid
-    ]
-
-
-@pytest.fixture
-def scanomaticd(tmpdir, scannerid):
+def scanomaticd(scannerid):
     dockerclient = docker.from_env()
     container = dockerclient.containers.run(
         'scanomaticd:latest',
@@ -70,18 +42,83 @@ def scanomaticd(tmpdir, scannerid):
     container.remove()
 
 
-def test(tmpdir, scanomaticd, scanjobid):
-    nbscans = 3
-    interval = timedelta(minutes=1)
-    begin = datetime.now()
-    scantimes = []
-    while datetime.now() - begin < (nbscans + 1) * interval:
-        if len(glob(str(tmpdir.join('scans', '*.tiff')))) > len(scantimes):
-            scantimes.append(datetime.now())
-        assert scanomaticd.status in {'created', 'running'}
+class ScanomaticGateway:
+    def __init__(self):
+        self._session = Session()
+        self._session.auth = (USERNAME, PASSWORD)
+
+    def create_scan_job(self, scannerid, interval, duration):
+        response = self._session.post(
+            APIROOT + '/scan-jobs',
+            json={
+                'name': uuid.uuid1().hex,
+                'interval': interval.total_seconds(),
+                'duration': duration.total_seconds(),
+                'scannerId': scannerid,
+            },
+        )
+        response.raise_for_status()
+        return response.json()['identifier']
+
+    def start_scan_job(self, jobid):
+        (self
+            ._session.post(self.mkurl('/scan-jobs/{}/start', jobid))
+            .raise_for_status())
+
+    def has_scanner(self, scannerid):
+        response = self._session.get(self.mkurl('/scanners/{}', scannerid))
+        if response.status_code == HTTPStatus.NOT_FOUND:
+            return False
+        response.raise_for_status()
+        return True
+
+    def get_scan_times(self, jobid):
+        response = self._session.get(self.mkurl('/scans'))
+        response.raise_for_status()
+        return [
+            (datetime
+                .strptime(obj['startTime'], '%Y-%m-%dT%H:%M:%SZ')
+                .replace(tzinfo=timezone.utc))
+            for obj in response.json() if obj['scanJobId'] == jobid
+        ]
+
+    def mkurl(self, pattern, *args):
+        return APIROOT + pattern.format(*args)
+
+
+@pytest.fixture
+def scanomatic():
+    return ScanomaticGateway()
+
+
+def test(tmpdir, scanomaticd, scannerid, scanomatic):
+    jitter = timedelta(minutes=1)  # time for the scanner to pick-up the job
+    scaninterval = timedelta(minutes=5)
+    duration = 2 * scaninterval + jitter
+
+    # We need to wait until the scanner has sent its first status update
+    waitbegin = datetime.now()
+    while datetime.now() - waitbegin < timedelta(minutes=1):
+        if scanomatic.has_scanner(scannerid):
+            break
         time.sleep(1)
-    scantimes = get_scan_times(scanjobid)
-    assert len(scantimes) == nbscans
-    assert scantimes[0] - begin < SCANDURATION
-    assert interval < scantimes[1] - begin < interval + SCANDURATION
-    assert 2 * interval < scantimes[2] - begin < 2 * interval + SCANDURATION
+    else:
+        pytest.fail('Timeout while waiting for the scanner to appear')
+
+    scanjobid = scanomatic.create_scan_job(scannerid, scaninterval, duration)
+    scanomatic.start_scan_job(scanjobid)
+    begin = datetime.now(timezone.utc)
+
+    # Waiting one more interval than necessary to make sure the scanning
+    # properly terminates
+    while datetime.now(timezone.utc) - begin < (3 * scaninterval + jitter):
+        time.sleep(10)
+        scanomaticd.reload()
+        assert scanomaticd.status in {'running'}
+
+    scantimes = scanomatic.get_scan_times(scanjobid)
+    scantimes.sort()
+    assert len(scantimes) == 3
+    assert scantimes[0] - begin < jitter
+    assert scantimes[1] - begin < scaninterval + jitter
+    assert scantimes[2] - begin < 2 * scaninterval + jitter
