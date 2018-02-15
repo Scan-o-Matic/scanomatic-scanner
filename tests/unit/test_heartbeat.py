0177bb3b0d422271416401603adfc2703ca5ded8
Fredrik Gustafsson <fredrik.gustafsson@molflow.com>
2018-02-15 12:29:09 +0100

    added keepalive check to heartbeat

--- tests/unit/test_heartbeat.py
+++ tests/unit/test_heartbeat.py
@@ -24,9 +24,15 @@ class TestScannerHeartbeat:
     def store(self):
         return ['Image 1', 'Image 55']
 
-    def test_heartbeat_good_response(self, daemon, apigateway, store):
+    @pytest.fixture
+    def scanner(self):
+        scanner = MagicMock()
+        scanner.get_devices.return_value = ['epson']
+        return scanner
+
+    def test_heartbeat_good_response(self, daemon, apigateway, store, scanner):
         now = datetime.now(tz=timezone.utc)
-        heartbeat = HeartbeatCommand(apigateway, store)
+        heartbeat = HeartbeatCommand(apigateway, store, scanner)
         daemon.get_start_time.return_value = now
         heartbeat(daemon)
         apigateway.update_status.assert_called_once_with(
@@ -34,49 +40,52 @@ class TestScannerHeartbeat:
             next_scheduled_scan=None,
             images_to_send=2,
             start_time=now,
+            devices=['epson'],
         )
 
     def test_heartbeat_bad_response_doesnt_raise(
-        self, daemon, apigateway, store,
-    ):
+            self, daemon, apigateway, store, scanner):
         apigateway.update_status.side_effect = APIError("")
-        heartbeat = HeartbeatCommand(apigateway, store)
+        heartbeat = HeartbeatCommand(apigateway, store, scanner)
         heartbeat(daemon)
 
-    def test_heartbeat_with_scheduled_scan(self, daemon, apigateway, store):
+    def test_heartbeat_with_scheduled_scan(
+            self, daemon, apigateway, store, scanner):
         now = datetime.now(tz=timezone.utc)
         scheduled = now + timedelta(minutes=20)
         daemon.get_next_scheduled_scan.return_value = scheduled
         daemon.get_start_time.return_value = now
-        heartbeat = HeartbeatCommand(apigateway, store)
+        heartbeat = HeartbeatCommand(apigateway, store, scanner)
         heartbeat(daemon)
         apigateway.update_status.assert_called_once_with(
             job=None,
             next_scheduled_scan=scheduled,
             images_to_send=2,
             start_time=now,
+            devices=['epson'],
         )
 
-    def test_heartbeat_with_job(self, daemon, apigateway, scanningjob, store):
+    def test_heartbeat_with_job(
+            self, daemon, apigateway, scanningjob, store, scanner):
         daemon.get_scanning_job.return_value = scanningjob
         now = datetime.now(tz=timezone.utc)
         daemon.get_start_time.return_value = now
-        heartbeat = HeartbeatCommand(apigateway, store)
+        heartbeat = HeartbeatCommand(apigateway, store, scanner)
         heartbeat(daemon)
         apigateway.update_status.assert_called_once_with(
             job=scanningjob.id,
             next_scheduled_scan=None,
             images_to_send=2,
             start_time=now,
+            devices=['epson'],
         )
 
     def test_hearbeat_with_no_images_to_send(
-        self, daemon, apigateway, scanningjob, store
-    ):
+            self, daemon, apigateway, scanningjob, store, scanner):
         daemon.get_scanning_job.return_value = scanningjob
         now = datetime.now(tz=timezone.utc)
         daemon.get_start_time.return_value = now
-        heartbeat = HeartbeatCommand(apigateway, store)
+        heartbeat = HeartbeatCommand(apigateway, store, scanner)
         store.clear()
         heartbeat(daemon)
         apigateway.update_status.assert_called_once_with(
@@ -84,4 +93,5 @@ class TestScannerHeartbeat:
             next_scheduled_scan=None,
             images_to_send=0,
             start_time=now,
+            devices=['epson'],
         )
